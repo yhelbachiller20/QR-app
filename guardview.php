b67ee9863a94483889972002ff5320f1113de1be
Mariel <yhelbachiller02@gmail.com>
2021-05-07 11:57:51 +0800

    Update guardview.php

--- guardview.php
+++ guardview.php
@@ -25,10 +25,31 @@ include "script/guardview-model.php";
                 <input type="text" name="txtBodyTemp"  placeholder="--.-">       
 			</div>
 <br>
+<label for="Guard Name">Guard Name</label>
+
+			<select name="Assigned Guard">
+				<option value="0">&nbsp &nbsp ----- </option>
+				<option value="SG">OIC Vizorro</option>
+				<option value="SG">SG Baldo</option>
+				<option value="SG">SG Martin</option>
+				<option value="LG">LG Bachiller</option>
+				<option value="LG">LG Maglente</option>
+			</select> 
+			&nbsp &nbsp &nbsp
+			<label for="Post">Guard Post</label>
+
+			<select name="Guard Post">
+				<option value="0">&nbsp &nbsp ----- </option>
+				<option value="1">Multi Purpose Building</option>
+				<option value="2">Baguyo Gate</option>
+				<option value="3">Main Gate</option>
+			</select> 
+			<br>
+			<br>
 		<div class="form-group">
                 <input type="submit" class="btn btn-primary" name="btnbodyTemp" value="Submit">
           </div>	
-
+			
     </div>  
 	</center>
 	</form>
