881854addedbfaf3cf26a1f5cfd217aece85edd2
Mariel <yhelbachiller02@gmail.com>
2021-05-08 17:26:20 +0800

    corrections
    Correction and edit from sir Andie

--- consent.php
+++ consent.php
@@ -4,11 +4,11 @@ include "common/header.php"; ?>
  
 		<div class="panel-body"></center>
 	<p>
-	<center><h5><b>Personal and contact information <br> shared through this app is used solely for contract tracing purposes. <br>
-no data will be shared to other parties without your consent.</h5></center></b>
-				<br>By Continuing use this app, I hereby authorize Assumption Antipolo to collect and process the data indicated for the purpose of effecting control of the COVID-19 infection.
-				<br>I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, 
-				<br> Bayanihan to Heal as One Act, to provide truthful information. *
+	<center><h5><b>Personal information <br> shared through this app is used solely for contract tracing purposes. <br>
+No data will be shared to other parties without your consent.</h5></center></b>
+				<br>By continuing use this app, I hereby authorize Assumption Antipolo to collect and process the data indicated for the purpose of effecting control of the COVID-19 infection.
+				I understand that my personal information is protected by RA 10173, Data Privacy Act of 2012, and that I am required by RA 11469, 
+				Bayanihan to Heal as One Act, to provide truthful information. *
 				</p> 
 
  
--- script/hc4-model - Copy.php
+++ script/hc4-model - Copy.php
@@ -1,13 +1,9 @@
 <?php
 if(isset($_POST['choices'])){
-	$_SESSION['hc4'] = ['Question4'=> "No"];
-	$url = $config["url_base"].'hc5.php';
+
+	
+		$_SESSION['hc4'] = ['Question4'=> "No"];	
+		$url = $config["url_base"].'hc5.php';
 		header('Location: '.$url);
 }	
-		if(isset($_POST['choices'])){
-		$_SESSION['hc4'] = ['Question4'=>" Yes"];
-		$_SESSION['hc4']=['yesTxt' => $_POST['yesTxt']];
-			$url = $config["url_base"].'not.php';
-
-		
-		}
+	
