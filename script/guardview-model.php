0f8f85744910d605b63a035eeb0fd934ef063c6f
Mariel <yhelbachiller02@gmail.com>
2021-05-11 10:50:24 +0800

    removed comment
    removed comment

--- script/guardview-model.php
+++ script/guardview-model.php
@@ -1,6 +1,5 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+
 $link = mysqli_connect("localhost", "root", "", "checklist");
  
 // Check connection
@@ -8,7 +7,6 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
  
-// Attempt update query execution
 $sql = "UPDATE visitor 
 SET 
     GuardName = '".$_POST['txtGuardName']."' , 
@@ -21,7 +19,6 @@ if(mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
-// Close connection
 mysqli_close($link);
 ?>
 <?php
