0bf37087911b7c92e015381ca3de30f44a6c7c28
Mariel <yhelbachiller02@gmail.com>
2021-05-11 15:28:01 +0800

    url direction
    rediction for employee and visitor

--- Employee.php
+++ Employee.php
@@ -1,31 +1,13 @@
-<?php $pageTitle = 'Please  fill out this'. "<br>".'Health Declaration form properly. '; ?>
-<?php 
-include 'common/header.php';
-include 'script/visitorlogs.php'; 
-?>
-
-
-
-<form action="" method="post">
-            <div class="form-group"><br />
-			 <label>&nbsp &nbsp Last Name:</label>
-                <input type="text" name="last_name" class="form-control" placeholder="Lastname">
-            </div> 
-			<div class="form-group">
-                <label> &nbsp &nbsp First Name:</label>
-                <input type="text" name="first_name" class="form-control" placeholder="Firstname">
-            </div> 
-				
-					<br>
-					<br>
-			
-				<input type="submit" class="btn btn-primary" name="btnNext" value="Next" style="float:right;margin-right: 12px;">
-				<input type="submit" class="btn btn-primary" name="back" value="Previous" style="float:left;padding-left: 12px;border-left-width: 5px;margin-left: 8px;margin-right: 0px;">
-			<br>
-			<br />
-				<center>
-			<a href="Statement.php">View Privacy Statement</a>.</p>
+<?php $pageTitle = "Assumption Antipolo";
+include "common/header.php"; ?>
+          <div class="panel-body">
+				<p style="font-size:30px"> <center> <h1> <u>Online Health Declaration Form</u></h1>
+				</p> 
+				<br>
+				 <form action="Esafety.php" method="post">
+				 <input type="submit" class="btn btn-primary" value="Start">
 				</center>
-    </div>   
-				</form>
-<?php include "common/footer.php";?>
+
+            </div>
+            
+<?php include "common/footer.php"; ?>
---
+++ Esafety.php
@@ -0,0 +1,13 @@
+<?php $pageTitle ="Safety Reminders";?>
+<?php include "common/header.php"; ?>
+			<div class="panel-body"><center>
+				<img src= "./safety3.png" style="max-width:100%;height:110;">
+				<br>
+				<center>
+				<br>
+				<br>
+				 <form action="Econsent.php" method="post">
+				 <input type="submit" class="btn btn-primary" value="Continue">
+				</center>
+				</div>
+<?php include "common/footer.php"; ?>
