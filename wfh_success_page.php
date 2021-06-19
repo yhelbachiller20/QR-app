852f94add33c89f5db9305122a1742398eac693d
Mariel <yhelbachiller02@gmail.com>
2021-06-19 20:15:45 +0800

    Change
    Thank you for submitting your response. change into Stay Safe!

--- wfh_success_page.php
+++ wfh_success_page.php
@@ -4,5 +4,5 @@ include "common/header.php";
 unset($_SESSION['employee']);
 ?>
 <h1>Record Successfully saved.</h1>
-<p>Thank you for submitting your response.</p>
+<p>Stay Safe!</p>
 <?php include "common/footer.php"; ?>
