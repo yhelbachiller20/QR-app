96d2c292460807581a7cc22994320109f90e59bb
Mariel <yhelbachiller02@gmail.com>
2021-06-21 16:47:45 +0800

    Update wfh_success_page.php
    add ok button for redirect in mainpage

--- wfh_success_page.php
+++ wfh_success_page.php
@@ -5,4 +5,11 @@ unset($_SESSION['employee']);
 ?>
 <h1>Record Successfully saved.</h1>
 <p>Stay Safe!</p>
+ <center> 
+	<form action="Employee.php" method="post">
+                <input type="submit" name="ok" value="ok">
+            </form>
+</center>
+<br>
+
 <?php include "common/footer.php"; ?>
