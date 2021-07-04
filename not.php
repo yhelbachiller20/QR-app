86196a6d4d93a73920f77488e681d627dc0f56eb
Ron <ronald.pedraja@gmail.com>
2021-07-04 12:05:28 +0300

    added filter for not.php

--- not.php
+++ not.php
@@ -1,21 +1,20 @@
-<?php $pageTitle="<h2>Online Health Declaration Form Completed!</h2><br>
+<?php $pageTitle = "<h2>Online Health Declaration Form Completed!</h2><br>
 
-";?>
-<?php include "common/header.php";?>
-<?php include "common/connect.php";?>
-<?php include "script/session_to_db.php";?>
+"; ?>
+<?php include "common/header.php"; ?>
+<?php include "common/connect.php"; ?>
+<?php include "script/session_to_db.php"; ?>
 
 <?php
+
 var_dump($dataToStore['log_type']);
 //$x = $_SESSION["log_type"]=30;
 //$y = $_SESSION["log_type"]=20;
 //var_dump($x !== $y);
 if
-($dataToStore['log_type'] ){
-echo "Please inform the clinic of your symptoms and consult your physician. Thank you.";
+ ($dataToStore['log_type'] == 30) {
+    echo "Please inform the clinic of your symptoms and consult your physician. Thank you.";
+} else {
+    echo "We apologize but you are not allowed to proceed and enter the Assumption Antipolo premises. This is In line with the health protocols of the school.";
 }
-else{
-echo  "We apologize but you are not allowed to proceed and enter the Assumption Antipolo premises. This is In line with the health protocols of the school.";
-}
-
 ?>
